4d0afbd10a27d4aea77d902c9a6361fc65cbdce2
Naresh123se <naresh132na@gmail.com>
2024-07-29 02:00:26 +0545

    LMS

---
+++ Library-Management-System/dbconn.php
@@ -0,0 +1,15 @@
+<?php
+//start or resume the session on the server-side
+session_start();
+/*Database port user password*/
+$dbservername = "localhost:3307";
+$dbusername = "root";
+$dbpassword = "";
+// Create connection
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword);
+// Check connection
+if (!$conn) {
+    echo "Connected unsuccessfully";
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
